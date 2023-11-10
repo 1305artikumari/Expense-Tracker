73710b2ba20a03027f240889da3e10bc964e615f
Arti Kumari <artikumari09011999@gmail.com>
2023-11-10 11:24:29 +0530

    Premium Account Features

---
+++ ExpenseTrackerAppAssignment10/middleware/authetication.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/users');
+const secretKey = process.env.SECRET_KEY;
+
+exports.authorization = async(request,response,next)=>{
+    try {
+        const token = request.headers.authorization;
+        // console.log(token);
+        const decode = jwt.verify(token, secretKey);
+       
+        const user = await User.findByPk(decode.userId);
+        request.user = user;
+        // console.log(user);
+        next();
+        
+        
+    } catch (error) {
+        if (error.name === 'TokenExpiredError') {
+            response.status(401).json({ message: 'Time out please sign in' });
+        } else {
+            console.log('Error:', error);
+            response.status(500).json({ message: 'Internal Server Error - please login again' });
+        }
+    }
+}
