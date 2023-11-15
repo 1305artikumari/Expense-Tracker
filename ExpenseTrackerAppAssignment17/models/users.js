50d4cd83627508ea5c9306eb5cfddadaf1cf2d1d
Arti Kumari <artikumari09011999@gmail.com>
2023-11-15 11:46:54 +0530

    Report Generation Backend (Premium Feature)

---
+++ ExpenseTrackerAppAssignment17/models/users.js
@@ -0,0 +1,33 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../config/db')
+const User = sequelize.define('User', {
+   id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true
+   },
+   name: {
+      type: Sequelize.STRING,
+      allowNull: false
+   },
+   email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true
+   },
+   password: {
+      type: Sequelize.TEXT,
+      allowNull: false
+   },
+   totalexpenses:{
+      type:Sequelize.FLOAT(),
+      defaultValue:0.00   
+  },
+  ispremiumuser:{
+      type: Sequelize.BOOLEAN,
+      defaultValue:false,
+  }
+
+})
+module.exports = User;
